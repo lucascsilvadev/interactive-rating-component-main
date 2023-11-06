3c2af8cf65813b15f00a8df45c04b76f1d2c5957
Lucas Costa da Silva <lucascsilvadev@outlook.com>
2023-11-06 19:58:50 -0300

    Add the warning feature

--- js/app.js
+++ js/app.js
@@ -1,7 +1,9 @@
 const options = document.querySelectorAll('.grades__option');
 const submitButton = document.querySelector('.grades__button');
 const radioButtons = document.querySelectorAll('.grades__radio');
-let selectedGrade;
+const gradesForm = document.querySelector('.grades');
+const warning = document.querySelector('.card__warning');
+let selectedGrade = '';
 
 options.forEach(option => {
     option.addEventListener('click', () => {
@@ -12,9 +14,17 @@ options.forEach(option => {
 
 })
 
-submitButton.addEventListener('submit', (e) => {
-    e.preventDefault();
-    console.log('submit');
+gradesForm.addEventListener('submit', (evento) => {
+    evento.preventDefault();
+    if (selectedGrade === '') { 
+        showWarning();
+
+    } else {
+        UpdatesGradesOptionsContent();
+        hideWarning(); 
+    }
+    
+    
 });
 
 
@@ -48,5 +58,59 @@ function updateSelectedRadioButton() {
             radioButton.checked = "true";
         }
     });
+}
+
+function UpdatesGradesOptionsContent() {
+    hideIcon();
+    switchText();
+    hideGrades();
+    hideSubmitButton();
+    addIllustration();
+    addResult();
+}
+
+function hideIcon() {
+    const icon = document.querySelector('.card__icon');
+    icon.setAttribute('style', 'display: none;')
+}
+
+function switchText() {
+    const title = document.querySelector('.card__title');
+    title.style.textAlign = 'center';
+    title.innerHTML = 'Thank you!';
+
+    const text = document.querySelector('.card__text');
+    text.style.textAlign = 'center';
+    text.innerHTML = 'We appreciate you taking the time to give a rating. If you ever need more support, don\'t hesitate to get in touch!'
+}
+
+function hideGrades() {
+    options.forEach(option => {
+        option.setAttribute('style', 'display: none;');
+    })
+}
+
+function hideSubmitButton() {
+    submitButton.style.display = 'none';
+}
+
+function addIllustration() {
+    const illustration = document.querySelector('.card__image');
+    illustration.style.display = 'inline-block';
+}
+
+function addResult() {
+    const result = document.querySelector('.card__result');
+    result.innerHTML = `You selected ${selectedGrade} out of 5`;
+    result.style.display = 'inline-block';
+    
+}
+
+function showWarning() {
+    warning.style.display = 'inline-block';
+}
+
+function hideWarning() {
+    warning.style.display = 'none';
 }
 
